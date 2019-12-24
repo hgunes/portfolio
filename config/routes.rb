a333a5a06dc720603713ee3e899f13ab24366ede
Harun Gunes <h.gunes13@gmail.com>
2019-12-24 22:24:28 +0100

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolio_res
+  resources :portfolio_res, except: [:show]
+  get 'portfolio/:id', to: 'portfolio_res#show', as: 'portfolio_show'
   root 'pages#home'
   get 'about', to:'pages#about'
   get 'contact', to: 'pages#contact'
   resources :blogs
-
-
 end
