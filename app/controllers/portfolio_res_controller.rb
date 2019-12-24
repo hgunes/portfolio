f7bb804fcc62f35f17bb1c0924a3a25105b78515
Harun Gunes <h.gunes13@gmail.com>
2019-12-24 21:12:38 +0100

    Integrated new and create actions for portfolio items

--- app/controllers/portfolio_res_controller.rb
+++ app/controllers/portfolio_res_controller.rb
@@ -2,4 +2,18 @@ class PortfolioResController < ApplicationController
   def index
     @portfolio_items = PortfolioRe.all
   end
+
+  def new
+    @portfolio_item = PortfolioRe.new
+  end
+
+  def create
+    @portfolio_item = PortfolioRe.new(params.require(:portfolio_re).permit(:title, :subtitle, :body))
+
+    if @portfolio_item.save
+      redirect_to portfolio_res_path
+    else
+      render :new
+    end
+  end
 end
