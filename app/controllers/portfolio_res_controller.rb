c8a8a3478a6368a89eadc72e184fcafa1606a564
Harun Gunes <h.gunes13@gmail.com>
2019-12-27 23:37:56 +0100

    Implemented form configuration for nested attributes

--- app/controllers/portfolio_res_controller.rb
+++ app/controllers/portfolio_res_controller.rb
@@ -17,10 +17,11 @@ class PortfolioResController < ApplicationController
 
   def new
     @portfolio_item = PortfolioRe.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
-    @portfolio_item = PortfolioRe.new(params.require(:portfolio_re).permit(:title, :subtitle, :body))
+    @portfolio_item = PortfolioRe.new(params.require(:portfolio_re).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     if @portfolio_item.save
       redirect_to portfolio_res_path
