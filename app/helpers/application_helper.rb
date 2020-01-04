8890f0acc89e69ebf93f91aae5ffa1e67e79f28d
Harun Gunes <h.gunes13@gmail.com>
2020-01-04 21:24:27 +0100

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+	def copyright_generator
+		Coprght::Renderer.copyright 'Harun Gunes', 'All rights reserved'
+	end
 end
