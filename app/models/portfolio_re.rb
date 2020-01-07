ebea5d50ea1a2adccce9723af3544a849c0f2b18
Harun Gunes <h.gunes13@gmail.com>
2020-01-07 20:56:52 +0100

    Implemented initial desing for portfolio and blog pages & updated seeds file

--- app/models/portfolio_re.rb
+++ app/models/portfolio_re.rb
@@ -16,6 +16,6 @@ class PortfolioRe < ApplicationRecord
 
   def set_defaults
     self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
-    self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
+    self.thumb_image ||= Placeholder.image_generator(height: 280, width: 180)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,23 +23,13 @@ end
 
 puts "5 skills created"
 
-8.times do |portfolio_item|
+9.times do |portfolio_item|
   PortfolioRe.create!(
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "Ruby on Rails",
     body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
     main_image: "https://placehold.it/600x400",
-    thumb_image: "https://placehold.it/350x200"
-  )
-end
-
-1.times do |portfolio_item|
-  PortfolioRe.create!(
-    title: "Portfolio title: #{portfolio_item}",
-    subtitle: "Angular",
-    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    main_image: "https://placehold.it/600x400",
-    thumb_image: "https://placehold.it/350x200"
+    thumb_image: "https://placehold.it/600x400"
   )
 end
 
